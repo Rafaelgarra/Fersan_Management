3722e3a45ae566e4a9cbadb293c473b8511e860e
Rafaelgarra <rafaelgaara1@gmail.com>
2025-12-31 16:53:21 -0300

    fix: test update

--- consolidar.py
+++ consolidar.py
@@ -14,8 +14,8 @@ from tkinter import scrolledtext, messagebox, ttk
 from packaging import version
 
 warnings.filterwarnings("ignore", category=UserWarning)
-
-VERSAO_ATUAL = "1.3.2"
+#Test
+VERSAO_ATUAL = "1.3.3"
 REPO_USER = "Rafaelgarra"
 REPO_NAME = "Fersan_Management"
 NOME_EXECUTAVEL = "RoboFersan.exe"
